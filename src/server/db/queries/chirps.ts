4c43b1015a865b281c0532eedd532781b6bd69d6
mendi <mendenhall_chris1246@yahoo.com>
2024-03-07 18:19:35 -0800

    fixed delete chirp query function

--- src/server/db/queries/chirps.ts
+++ src/server/db/queries/chirps.ts
@@ -5,7 +5,7 @@ const getAll = () => Query<IChirp[]>("SELECT * FROM chirps");
 const getOne = (id: number) => Query<IChirp[]>("SELECT * FROM chirps WHERE id=?", [id]);
 const create = (newChirp: IBaseChirp) => Query("INSERT INTO chirps SET ?", [newChirp]);
 const update = (updatedChirp: IBaseChirp, id: number) => Query("UPDATE chirps SET ? WHERE id=?", [updatedChirp, id]);
-const destroy = (id: number) => Query("DELETE chirps WHERE id=?", [id]);
+const destroy = (id: number) => Query("DELETE FROM chirps WHERE id=?", [id]);
 
 export default {
 	getAll,
