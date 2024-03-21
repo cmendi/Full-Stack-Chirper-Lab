bb531b24e004d3636c8df0d29c04461faf57658d
unknown <mendenhall_chris1246@yahoo.com>
2024-03-20 20:49:47 -0700

    fixed formatting

--- src/client/views/ErrorPage.tsx
+++ src/client/views/ErrorPage.tsx
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
 	const nav = useNavigate();
+
 	useEffect(() => {
 		setTimeout(() => {
 			nav("/");
 		}, 5000);
 	});
+
 	return (
 		<h1 className="text-danger text-center mt-5">No such page exists</h1>
 	);
