c966890f17add901cbac939b1fdfdeb7babc178f
mendi <mendenhall_chris1246@yahoo.com>
2024-03-23 10:47:42 -0700

    fixed chirp details styling

--- src/client/views/ChirpDetails.tsx
+++ src/client/views/ChirpDetails.tsx
@@ -7,7 +7,6 @@ const ChirpDetails = () => {
 	const { id } = useParams();
 	const [chirp, setChirp] = useState<IChirp>();
 
-	// Whats the reason to put the id in the useEffect dependancy array?
 	useEffect(() => {
 		GET(`/api/chirps/${id}`).then(setChirp);
 	}, [id]);
@@ -18,16 +17,18 @@ const ChirpDetails = () => {
 
 	return (
 		<div className="row justify-content-center">
-			<div className="card p-3 shadow-lg my-2 bg-white">
-				<p className="fs-5">{chirp.body}</p>
+			<div className="col-12 col-md-7">
+				<div className="card p-3 my-2 shadow-lg bg-white">
+					<p className="fs-5">{chirp.body}</p>
 
-				<p>Created {new Date(chirp.created_at).toLocaleString()}</p>
+					<p>Created {new Date(chirp.created_at).toLocaleString()}</p>
 
-				<div className="card-footer">
-					<p>Chirped from {chirp.location ? chirp.location : "Wouldn't you like to know."}</p>
-					<Link to={`/chirps/${chirp.id}/edit`} className="btn btn-info m-1">
-						Edit Chirp
-					</Link>
+					<div className="card-footer">
+						<p>Chirped from {chirp.location ? chirp.location : "Wouldn't you like to know."}</p>
+						<Link to={`/chirps/${chirp.id}/edit`} className="btn btn-info m-1">
+							Edit Chirp
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
